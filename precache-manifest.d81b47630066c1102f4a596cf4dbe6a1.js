d4c357a375b195830cfc08ce7bcd9c4d3a9542c0
Radu Rizea <rereradu@gmail.com>
2020-11-13 09:19:16 +0000

    deploy

---
+++ precache-manifest.d81b47630066c1102f4a596cf4dbe6a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ef0154ec3c79755b78",
+    "url": "/vuejs-test/css/app.dca94a59.css"
+  },
+  {
+    "revision": "ee3367d8a053b3d53a99",
+    "url": "/vuejs-test/css/chunk-vendors.980c29a1.css"
+  },
+  {
+    "revision": "34625302b9c9d32c5aa8a423c3401de9",
+    "url": "/vuejs-test/index.html"
+  },
+  {
+    "revision": "70ef0154ec3c79755b78",
+    "url": "/vuejs-test/js/app.2c22de30.js"
+  },
+  {
+    "revision": "ee3367d8a053b3d53a99",
+    "url": "/vuejs-test/js/chunk-vendors.9549e5a7.js"
+  },
+  {
+    "revision": "e6a6be9dd69109daba5dc8e04fa5e811",
+    "url": "/vuejs-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuejs-test/robots.txt"
+  }
+]);
